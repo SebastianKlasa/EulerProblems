43b84c8ff663586ab71e2ace600948f430728b81
SebastianKlasa <sebastianklasa1@gmail.com>
2021-02-11 19:06:04 +0100

    euler54 tested and working

--- src/main/java/EulerProblems/Euler54.java
+++ src/main/java/EulerProblems/Euler54.java
@@ -5,16 +5,24 @@ import java.util.*;
 
 public class Euler54 {
     public static enum RANK{
-        highCard,
-        onePair,
-        twoPairs,
-        treeOfAKind,
-        Straight,
-        flush,
-        fullHouse,
-        fourOfAKind,
-        StraightFlush,
-        RoyalFlush
+        highCard(1),
+        onePair(2),
+        twoPairs(3),
+        treeOfAKind(4),
+        straight(5),
+        flush(6),
+        fullHouse(7),
+        fourOfAKind(8),
+        straightFlush(9),
+        royalFlush(10);
+
+        private final int value;
+        private RANK(int value){
+            this.value = value;
+        }
+        public int getValue(){
+            return value;
+        }
     }
 
 
@@ -45,13 +53,32 @@ public class Euler54 {
         public int getStrenght() {
             return strenght;
         }
+
+        @Override
+        public int hashCode() {
+            return value + strenght;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            Card card = (Card)obj;
+            if(this.getValue() == card.getValue()) return true;
+            else return false;
+        }
     }
 
     public static class PlayerCards{
         List<Card> cards;
 
+        /**
+         * card with index 0 is the most valuable
+         * card with index n is the least valuable
+         */
+        List<Card> highestRankedCard;
+
         public PlayerCards() {
             cards = new ArrayList<Card>();
+            highestRankedCard = new ArrayList<Card>();
         }
 
         public void addCard(Card card){
@@ -63,8 +90,105 @@ public class Euler54 {
             return cards;
         }
 
+        public RANK getCardsRank(){
+            if(isRoyalFlush()) return RANK.royalFlush;
+            else if(isStraightFlush()) return RANK.straightFlush;
+            else if(isFourOfAKind()) return RANK.fourOfAKind;
+            else if(isFullHouse()) return RANK.fullHouse;
+            else if(isFlush()) return RANK.flush;
+            else if(isStraight()) return RANK.straight;
+            else if(isThreeOfAKind()) return RANK.treeOfAKind;
+            else if(isTwoPairs()) return RANK.twoPairs;
+            else if(isSOnePair()) return RANK.onePair;
+            else{
+                for(int i = 4; i>=0; i--){
+                    highestRankedCard.add(cards.get(i));
+                }
+                return RANK.highCard;
+            }
+        }
+
+        /**
+         *
+         * @param n high card turn
+         * @return if n=4 return highest card, if n=0 lowest
+         */
+        public Card getHighCard(int n){
+            return cards.get(n);
+        }
+
+        public List<Card> getHighestRankedCard(){
+            return highestRankedCard;
+        }
+
         private boolean isRoyalFlush(){
-            return true;
+            if(isStraightFlush() && getHighCard(0).getValue() == 'T'){
+                highestRankedCard.add(cards.get(4));
+                return true;
+            }
+            return false;
+        }
+
+        private boolean isStraightFlush(){
+            if(isFlush() && isStraight()) {
+                highestRankedCard.add(cards.get(4));
+                return true;
+            }
+            return false;
+        }
+
+        /**
+         *
+         * @param set
+         * @param exception1 is a first value which you don't want to return
+         * @param exception2 is a second value which you don't want to return
+         * @return
+         */
+        private Card getSetValue(Set<Card> set, char exception1, char exception2){
+            Card cardValue = new Card("0X");
+            for(Iterator<Card> it = set.iterator(); it.hasNext();){
+                Card tmp = it.next();
+                if(tmp.getStrenght() > cardValue.getStrenght() && tmp.getValue() != exception1 && tmp.getValue() != exception2) {
+                    cardValue = tmp;
+                }
+            }
+            return cardValue;
+        }
+
+        private boolean isFourOfAKind(){
+            HashSet<Card> p = new HashSet<>();
+            HashSet<Card> avoided = new HashSet<>();
+            for(Card c:cards){
+                if(!p.add(c)){
+                    avoided.add(c);
+                }
+            }
+            if(p.size() == 2 && avoided.size() == 1){
+                Card fourValue = getSetValue(avoided, '0', '0');
+                Card oneValue = getSetValue(p, fourValue.getValue(), '0');
+                highestRankedCard.add(fourValue);
+                highestRankedCard.add(oneValue);
+                return true;
+            }
+            return false;
+        }
+
+        private boolean isFullHouse(){
+            Set<Card> p = new HashSet<>();
+            Set<Card> avoided = new HashSet<>();
+            for(Card c:cards){
+                if(!p.add(c)){
+                    avoided.add(c);
+                }
+            }
+            if(p.size() == 2 && avoided.size() == 2){
+                Card higerValue = getSetValue(avoided, '0', '0');
+                Card oneValue = getSetValue(p, higerValue.getValue(), '0');
+                highestRankedCard.add(higerValue);
+                highestRankedCard.add(oneValue);
+                return true;
+            }
+            return false;
         }
 
         private boolean isFlush(){
@@ -72,13 +196,103 @@ public class Euler54 {
             for(Card card:cards){
                 if(card.getColor()!=color) return false;
             }
+            for(int i=4; i>=0; i--){
+                highestRankedCard.add(cards.get(i));
+            }
             return true;
         }
 
         private boolean isStraight(){
+            int currStrength = cards.get(0).getStrenght();
+            for(int i=1; i<cards.size(); i++){
+                if(cards.get(i).getStrenght() != (currStrength+1)) return false;
+                currStrength = cards.get(i).getStrenght();
+            }
+            highestRankedCard.add(cards.get(4));
             return true;
         }
 
+        private boolean isThreeOfAKind(){
+            Set<Card> p = new HashSet<>();
+            Set<Card> avoided = new HashSet<>();
+            for(Card c:cards){
+                if(!p.add(c)){
+                    avoided.add(c);
+                }
+            }
+            if(p.size() == 3 && avoided.size() == 1){
+                Card threeValue = getSetValue(avoided, '0', '0');
+                Card oneHigherValue = getSetValue(p, threeValue.getValue(), '0');
+                Card oneLowerValue = getSetValue(p, threeValue.getValue(), oneHigherValue.getValue());
+                highestRankedCard.add(threeValue);
+                highestRankedCard.add(oneHigherValue);
+                highestRankedCard.add(oneLowerValue);
+                return true;
+            }
+            return false;
+        }
+
+        private boolean isTwoPairs(){
+            Set<Card> p = new HashSet<>();
+            Set<Card> avoided = new HashSet<>();
+            for(Card c:cards){
+                if(!p.add(c)){
+                    avoided.add(c);
+                }
+            }
+            if(p.size() == 3 && avoided.size() == 2){
+                Card higherPair = getSetValue(avoided, '0', '0');
+                Card lowerPair = getSetValue(avoided, higherPair.getValue(), '0');
+                Card oneLowerValue = getSetValue(p, higherPair.getValue(), lowerPair.getValue());
+                highestRankedCard.add(higherPair);
+                highestRankedCard.add(lowerPair);
+                highestRankedCard.add(oneLowerValue);
+                return true;
+            }
+            return false;
+        }
+
+        private boolean isSOnePair(){
+            Set<Card> p = new HashSet();
+            Set<Card> avoided = new HashSet<>();
+            for(Card c:cards){
+                if(!p.add(c)){
+                    avoided.add(c);
+                }
+            }
+            if(p.size() == 4){
+                Card pair = getSetValue(avoided, '0', '0');
+                highestRankedCard.add(pair);
+                for(int i=4; i>=0; i--){
+                    Card tmp = cards.get(i);
+                    if(!tmp.equals(pair)) {
+                        highestRankedCard.add(tmp);
+                    }
+                }
+
+                return true;
+            }
+            return false;
+        }
+
+    }
+
+    public static int getWinner(PlayerCards pl1, PlayerCards pl2){
+        RANK rankPl1 = pl1.getCardsRank();
+        RANK rankPl2 = pl2.getCardsRank();
+        if(rankPl1.getValue() > rankPl2.getValue()) return 1;
+        else if(rankPl1.getValue() < rankPl2.getValue()) return 2;
+        else{
+            //wysoka karta w ukladzie
+            int n=4;
+            List<Card> pl1Cards = pl1.getHighestRankedCard();
+            List<Card> pl2Cards = pl2.getHighestRankedCard();
+            for (int i=0;i<pl1Cards.size(); i++){
+                if(pl1Cards.get(i).getStrenght() > pl2Cards.get(i).getStrenght()) return 1;
+                else if(pl1Cards.get(i).getStrenght() < pl2Cards.get(i).getStrenght()) return 2;
+            }
+        }
+        return 0;
     }
 
     public static void main(String[] args) {
@@ -87,6 +301,8 @@ public class Euler54 {
 
         try(BufferedReader br = new BufferedReader(new FileReader("poker.txt"))){
             String line;
+            int player1Wins = 0;
+            int player2Wins = 0;
             while((line = br.readLine()) != null){
                 String[] splitted = line.split(" ");
                 PlayerCards pl1 = new PlayerCards();
@@ -98,7 +314,18 @@ public class Euler54 {
                 }
                 player1.add(pl1);
                 player2.add(pl2);
+                int winner = getWinner(pl1, pl2);
+                switch (winner) {
+                    case 1: player1Wins++;
+                    break;
+                    case 2: player2Wins++;
+                    break;
+                }
             }
+            System.out.println("Player1 wins: ");
+            System.out.println(player1Wins);
+            System.out.println("Player2 wins: ");
+            System.out.println(player2Wins);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
--- src/test/java/PokerTest.java
+++ src/test/java/PokerTest.java
@@ -13,9 +13,27 @@ import static org.junit.Assert.*;
 @RunWith(JUnit4.class)
 public class PokerTest {
     public static Euler54.PlayerCards pl1;    //player with flush
+    public static Euler54.PlayerCards pl2;    //player with straight
+    public static Euler54.PlayerCards pl3;    //player with straight flush
+    public static Euler54.PlayerCards pl4;    //player with royal flush
+    public static Euler54.PlayerCards pl5;    //player with pair
+    public static Euler54.PlayerCards pl6;    //player with two pairs
+    public static Euler54.PlayerCards pl7;    //player with three of a kind
+    public static Euler54.PlayerCards pl8;    //player with full house
+    public static Euler54.PlayerCards pl9;    //player with four of a kind
+
+    public static Method isRoyalFlush;
+    public static Method isStraightFlush;
+    public static Method isFourOfAKind;
+    public static Method isFullHouse;
+    public static Method isFlush;
+    public static Method isStraight;
+    public static Method isThreeOfAKind;
+    public static Method isTwoPairs;
+    public static Method isSOnePair;
 
     @BeforeClass
-    public static void init(){
+    public static void initFlush(){
         Euler54.Card c1 = new Euler54.Card("QD");
         Euler54.Card c2 = new Euler54.Card("5D");
         Euler54.Card c3 = new Euler54.Card("6D");
@@ -27,6 +45,143 @@ public class PokerTest {
         pl1.addCard(c3);
         pl1.addCard(c4);
         pl1.addCard(c5);
+    }
+
+    @BeforeClass
+    public static void initStright(){
+        Euler54.Card c1 = new Euler54.Card("8D");
+        Euler54.Card c2 = new Euler54.Card("TC");
+        Euler54.Card c3 = new Euler54.Card("QD");
+        Euler54.Card c4 = new Euler54.Card("JD");
+        Euler54.Card c5 = new Euler54.Card("9S");
+        pl2 = new Euler54.PlayerCards();
+        pl2.addCard(c1);
+        pl2.addCard(c2);
+        pl2.addCard(c3);
+        pl2.addCard(c4);
+        pl2.addCard(c5);
+    }
+
+    @BeforeClass
+    public static void initStrightFlush(){
+        Euler54.Card c1 = new Euler54.Card("5H");
+        Euler54.Card c2 = new Euler54.Card("3H");
+        Euler54.Card c3 = new Euler54.Card("2H");
+        Euler54.Card c4 = new Euler54.Card("6H");
+        Euler54.Card c5 = new Euler54.Card("4H");
+        pl3 = new Euler54.PlayerCards();
+        pl3.addCard(c1);
+        pl3.addCard(c2);
+        pl3.addCard(c3);
+        pl3.addCard(c4);
+        pl3.addCard(c5);
+    }
+
+    @BeforeClass
+    public static void initRoyalFlush(){
+        Euler54.Card c1 = new Euler54.Card("TD");
+        Euler54.Card c2 = new Euler54.Card("AD");
+        Euler54.Card c3 = new Euler54.Card("QD");
+        Euler54.Card c4 = new Euler54.Card("JD");
+        Euler54.Card c5 = new Euler54.Card("KD");
+        pl4 = new Euler54.PlayerCards();
+        pl4.addCard(c1);
+        pl4.addCard(c2);
+        pl4.addCard(c3);
+        pl4.addCard(c4);
+        pl4.addCard(c5);
+    }
+
+    @BeforeClass
+    public static void initPair(){
+        Euler54.Card c1 = new Euler54.Card("TS");
+        Euler54.Card c2 = new Euler54.Card("AD");
+        Euler54.Card c3 = new Euler54.Card("QD");
+        Euler54.Card c4 = new Euler54.Card("JH");
+        Euler54.Card c5 = new Euler54.Card("TD");
+        pl5 = new Euler54.PlayerCards();
+        pl5.addCard(c1);
+        pl5.addCard(c2);
+        pl5.addCard(c3);
+        pl5.addCard(c4);
+        pl5.addCard(c5);
+    }
+
+    @BeforeClass
+    public static void initTwoPair(){
+        Euler54.Card c1 = new Euler54.Card("TS");
+        Euler54.Card c2 = new Euler54.Card("2D");
+        Euler54.Card c3 = new Euler54.Card("2D");
+        Euler54.Card c4 = new Euler54.Card("JH");
+        Euler54.Card c5 = new Euler54.Card("TD");
+        pl6 = new Euler54.PlayerCards();
+        pl6.addCard(c1);
+        pl6.addCard(c2);
+        pl6.addCard(c3);
+        pl6.addCard(c4);
+        pl6.addCard(c5);
+    }
+
+    @BeforeClass
+    public static void initThreeOfAKind(){
+        Euler54.Card c1 = new Euler54.Card("8S");
+        Euler54.Card c2 = new Euler54.Card("3D");
+        Euler54.Card c3 = new Euler54.Card("3D");
+        Euler54.Card c4 = new Euler54.Card("3H");
+        Euler54.Card c5 = new Euler54.Card("TD");
+        pl7 = new Euler54.PlayerCards();
+        pl7.addCard(c1);
+        pl7.addCard(c2);
+        pl7.addCard(c3);
+        pl7.addCard(c4);
+        pl7.addCard(c5);
+    }
+
+    @BeforeClass
+    public static void initFullHouse(){
+        Euler54.Card c1 = new Euler54.Card("8S");
+        Euler54.Card c2 = new Euler54.Card("3D");
+        Euler54.Card c3 = new Euler54.Card("8D");
+        Euler54.Card c4 = new Euler54.Card("3H");
+        Euler54.Card c5 = new Euler54.Card("8D");
+        pl8 = new Euler54.PlayerCards();
+        pl8.addCard(c1);
+        pl8.addCard(c2);
+        pl8.addCard(c3);
+        pl8.addCard(c4);
+        pl8.addCard(c5);
+    }
+
+    @BeforeClass
+    public static void initFourOfAKind(){
+        Euler54.Card c1 = new Euler54.Card("QS");
+        Euler54.Card c2 = new Euler54.Card("3D");
+        Euler54.Card c3 = new Euler54.Card("QD");
+        Euler54.Card c4 = new Euler54.Card("QH");
+        Euler54.Card c5 = new Euler54.Card("QD");
+        pl9 = new Euler54.PlayerCards();
+        pl9.addCard(c1);
+        pl9.addCard(c2);
+        pl9.addCard(c3);
+        pl9.addCard(c4);
+        pl9.addCard(c5);
+    }
+
+    @BeforeClass
+    public static void initMethods(){
+        try {
+            isRoyalFlush = Euler54.PlayerCards.class.getDeclaredMethod("isRoyalFlush", null);
+            isStraightFlush = Euler54.PlayerCards.class.getDeclaredMethod("isStraightFlush", null);
+            isFourOfAKind = Euler54.PlayerCards.class.getDeclaredMethod("isFourOfAKind", null);
+            isFullHouse = Euler54.PlayerCards.class.getDeclaredMethod("isFullHouse", null);
+            isFlush= Euler54.PlayerCards.class.getDeclaredMethod("isFlush", null);
+            isStraight = Euler54.PlayerCards.class.getDeclaredMethod("isStraight", null);;
+            isThreeOfAKind = Euler54.PlayerCards.class.getDeclaredMethod("isThreeOfAKind", null);
+            isTwoPairs = Euler54.PlayerCards.class.getDeclaredMethod("isTwoPairs", null);
+            isSOnePair = Euler54.PlayerCards.class.getDeclaredMethod("isSOnePair", null);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
@@ -46,14 +201,13 @@ public class PokerTest {
 
     @Test
     public void TestFlush(){
-        Method method = null;
         try {
-            method = Euler54.PlayerCards.class.getDeclaredMethod("isFlush", null);
-            method.setAccessible(true);
-            Object b = method.invoke(pl1, null);
+            isFlush.setAccessible(true);
+            Object b = isFlush.invoke(pl1, null);
             assertTrue((Boolean) b);
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
+
+            Object b2 = isFlush.invoke(pl4, null);
+            assertTrue((Boolean)b2);
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         } catch (InvocationTargetException e) {
@@ -65,6 +219,231 @@ public class PokerTest {
     public void TestSorted(){
         List<Euler54.Card> cards = pl1.getCards();
         assertEquals(cards.get(3).getStrenght(), 12);
+    }
+
+    @Test
+    public void TestStraight(){
+        try {
+            isStraight.setAccessible(true);
+            Object b = isStraight.invoke(pl2, null);
+            assertTrue((Boolean) b);
+
+            Object b2 = isStraight.invoke(pl4, null);
+            assertTrue((Boolean)b2);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void TestStraightFlush(){
+        try {
+            isStraightFlush.setAccessible(true);
+            Object b = isStraightFlush.invoke(pl3, null);
+            assertTrue((Boolean) b);
+
+            Object b2 = isStraightFlush.invoke(pl4, null);
+            assertTrue((Boolean)b2);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void TestRoyalFlush(){
+        try {
+            isRoyalFlush.setAccessible(true);
+            Object b = isRoyalFlush.invoke(pl4, null);
+            assertTrue((Boolean) b);
+
+            isRoyalFlush.setAccessible(true);
+            Object b2 = isRoyalFlush.invoke(pl3, null);
+            assertFalse((Boolean) b2);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void TestPair(){
+        try {
+            isSOnePair.setAccessible(true);
+            Object b = isSOnePair.invoke(pl5, null);
+            assertTrue((Boolean) b);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void TestTwoPair(){
+        try {
+            isTwoPairs.setAccessible(true);
+            Object b = isTwoPairs.invoke(pl6, null);
+            assertTrue((Boolean) b);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void TestThreeOfAKind(){
+        try {
+            isThreeOfAKind.setAccessible(true);
+            Object b = isThreeOfAKind.invoke(pl7, null);
+            assertTrue((Boolean) b);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void TestFullHouse(){
+        try {
+            isFullHouse.setAccessible(true);
+            Object b = isFullHouse.invoke(pl8, null);
+            assertTrue((Boolean) b);
+        }  catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void TestFourOfAKind(){
+        try {
+            isFourOfAKind.setAccessible(true);
+            Object b = isFourOfAKind.invoke(pl9, null);
+            assertTrue((Boolean) b);
+        }  catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void TestHighCard1(){
+        pl1.getCardsRank();
+        Euler54.Card c1 = pl1.getHighestRankedCard().get(0);
+        Euler54.Card c3 = pl1.getHighestRankedCard().get(2);
+        Euler54.Card c5 = pl1.getHighestRankedCard().get(4);
+        assertEquals(c1.getValue(), 'K');
+        assertEquals(c3.getValue(), '7');
+        assertEquals(c5.getValue(), '5');
+    }
+
+    @Test
+    public void TestHighCard2(){
+        Euler54.Card c2 = pl2.getHighestRankedCard().get(0);
+        assertEquals(c2.getValue(), 'Q');
+    }
+
+    @Test
+    public void TestHighCard3(){
+        Euler54.Card c3 = pl3.getHighestRankedCard().get(0);
+        assertEquals(c3.getValue(), '6');
+    }
+
+    @Test
+    public void TestHighCard4(){
+        Euler54.Card c4 = pl4.getHighestRankedCard().get(0);
+        assertEquals(c4.getValue(), 'A');
+    }
+
+    @Test
+    public void TestHighCard5(){
+        Euler54.Card c5 = pl5.getHighestRankedCard().get(0);
+        Euler54.Card c52 = pl5.getHighestRankedCard().get(3);
+        assertEquals(c5.getValue(), 'T');
+        assertEquals(c52.getValue(), 'J');
+    }
+
+    @Test
+    public void TestHighCard6(){
+        Euler54.Card c6 =  pl6.getHighestRankedCard().get(0);
+        Euler54.Card c62 =  pl6.getHighestRankedCard().get(2);
+        assertEquals(c6.getValue(), 'T');
+        assertEquals(c62.getValue(), 'J');
+    }
+
+    @Test
+    public void TestHighCard7(){
+        pl7.getCardsRank();
+        Euler54.Card c7 = pl7.getHighestRankedCard().get(0);
+        Euler54.Card c72 = pl7.getHighestRankedCard().get(1);
+        assertEquals(c7.getValue(), '3');
+        assertEquals(c72.getValue(), 'T');
+    }
+
+    @Test
+    public void TestHighCard8(){
+        Euler54.Card c8 =   pl8.getHighestRankedCard().get(0);
+        Euler54.Card c82 =   pl8.getHighestRankedCard().get(1);
+        assertEquals(c8.getValue(), '8');
+        assertEquals(c82.getValue(), '3');
+    }
+
+    @Test
+    public void TestHighCard9(){
+        Euler54.Card c9 =   pl9.getHighestRankedCard().get(0);
+        Euler54.Card c92 =   pl9.getHighestRankedCard().get(1);
+        assertEquals(c92.getValue(), '3');
+    }
+
+    /**
+     *  Euler54.PlayerCards pl1;    //player with flush
+     *  Euler54.PlayerCards pl2;    //player with straight
+     *  Euler54.PlayerCards pl3;    //player with straight flush
+     *  Euler54.PlayerCards pl4;    //player with royal flush
+     *  Euler54.PlayerCards pl5;    //player with pair
+     *  Euler54.PlayerCards pl6;    //player with two pairs
+     *  Euler54.PlayerCards pl7;    //player with three of a kind
+     *  Euler54.PlayerCards pl8;    //player with full house
+     *  Euler54.PlayerCards pl9;    //player with four of a kind
+     */
+
+    @Test
+    public void TestWinner1(){
+        int winner = Euler54.getWinner(pl1, pl2);
+        assertEquals(winner, 1);
+    }
+
+    @Test
+    public void TestWinner2(){
+        int winner = Euler54.getWinner(pl9, pl5);
+        assertEquals(winner, 1);
+    }
+
+    @Test
+    public void TestWinner3(){
+        int winner = Euler54.getWinner(pl8, pl4);
+        assertEquals(winner, 2);
+    }
+
+    @Test
+    public void TestWinner4(){
+        int winner = Euler54.getWinner(pl5, pl6);
+        assertEquals(winner, 2);
+    }
+
+    @Test
+    public void TestWinner5(){
+        int winner = Euler54.getWinner(pl6, pl2);
+        assertEquals(winner, 2);
     }
 
 }
